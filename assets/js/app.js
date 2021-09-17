5939c4860585d3db1fa69c98667ed13b039c206e
alfredbaudisch <alfred@alfredbaudisch.com>
2021-09-17 09:06:21 +0200

    Removed local bootstrap files in favor of serving from CDN

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.css"
-import "./bootstrap.bundle"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
