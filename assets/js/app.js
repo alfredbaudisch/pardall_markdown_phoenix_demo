941b511887f0bcafbd7842080b682516c53c5963
alfredbaudisch <alfred@alfredbaudisch.com>
2021-09-23 13:48:19 +0200

    Added topbar for better UX; use pardall_markdown 0.3.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,7 @@ import "../css/app.css"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+import topbar from "topbar"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 
@@ -20,9 +21,9 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
-// topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-// window.addEventListener("phx:page-loading-start", info => topbar.show())
-// window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
