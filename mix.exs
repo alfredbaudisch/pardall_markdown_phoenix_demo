e87a79fb024c645b477eb9de89a1d312b213e914
Alfred Reinold Baudisch <alfred@alfredbaudisch.com>
2021-10-30 17:18:36 +0200

    Use the most up to date PardallMarkdown version, which fixes race conditions between Git and FileWatcher

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule PardallWeb.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
-      {:pardall_markdown, "~> 0.4.0"}
+      {:pardall_markdown, "~> 0.4"}
     ]
   end
 
